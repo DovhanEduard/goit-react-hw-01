8a1a8749d852f759f855cb6506eb285a5b06eb76
Dovhan_Eduard <dovhan.eduard@gmail.com>
2024-07-20 21:41:18 +0300

    add 2 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
-import userData from "./userData.json"
 import './App.css'
-import Profile from './Profile'
+import Profile from './Profile/Profile.jsx'
+import userData from "./userData.json"
+import FriendList from './FriendList/FriendList.jsx'
+import friendsList from "./friendsList.json"
 
 
 function App() {
@@ -13,7 +15,11 @@ function App() {
         image={userData.avatar}
         stats={userData.stats}
       />
+
+      <FriendList friends = {friendsList} />
+      
     </>
+    
   )
 }
 
